5f4a7f00ea072b469ab83794d1cd37b6ed1f8d79
Luis Gonzalez <Luisargo2123@gmail.com>
2020-02-26 14:45:43 -0500

    Added some functionality to read xml file and process it - will probably switch to json

--- datr.py
+++ datr.py
@@ -1,1 +1,10 @@
-print('Hello World')
+import requests
+from xml.etree import ElementTree
+print('hello world')
+r = requests.get('http://patcount.fiu.edu/garagecount.xml')
+tree = ElementTree.fromstring(r.content)
+print(tree[0])
+
+for elem in tree.iter():
+    print(elem.tag)
+    print(elem.text)
