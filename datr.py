2c43b829213ff23af82c83997529e5bdd4b479cd
Luis Gonzalez <Luisargo2123@gmail.com>
2020-02-29 13:14:38 -0500

    Added some trivial input processing

--- datr.py
+++ datr.py
@@ -8,3 +8,8 @@ print(tree[0])
 for elem in tree.iter():
     print(elem.tag)
     print(elem.text)
+
+distanceFilter = input('How far are you willing to drive (in miles): ')
+print(distanceFilter)
+budgetFilter = input('How much are you willing to spend (in USD): ')
+print(budgetFilter)
