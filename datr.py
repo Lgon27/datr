7203616f3e402b2acb0c5ed307c87635b668970b
Luis Gonzalez <Luisargo2123@gmail.com>
2020-03-02 17:39:04 -0500

    Added some json reading/writing functionality from a stackabuse tutorial. TODO: Modify this read/write functionality for my application's needs

--- datr.py
+++ datr.py
@@ -1,4 +1,5 @@
 import requests
+import json
 from xml.etree import ElementTree
 print('hello world')
 r = requests.get('http://patcount.fiu.edu/garagecount.xml')
@@ -19,9 +20,30 @@ while(exit != True):
         exit = True
     elif(chooseActivity == '1'):
         print('Adding Date')
+        data = {}
+        data['people'] = []
+        data['people'].append({
+            'name': 'Scott',
+            'website': 'Stackabuse.com',
+            'from': 'Nebraska'
+        })
+        data['people'].append({
+            'name': 'Larry',
+            'website': 'google.com',
+            'from': 'Michigan'
+        })
+        with open('data.txt', 'w') as outfile:
+            json.dump(data, outfile)
     elif(chooseActivity == '2'):
         print('Adding Food')
     elif(chooseActivity == '3'):
         print('Selecting Date')
+        with open('data.txt') as json_file:
+            data = json.load(json_file)
+            for p in data['people']:
+                print('Name: ' + p['name'])
+                print('Website: ' + p['website'])
+                print('From: ' + p['from'])
+                print('')
     else:
         print('Incorrect input: Valid input is: \n Add Date  (1) \n Add Food  (2) \n Pick Date (3)\n Exit      (4)')
