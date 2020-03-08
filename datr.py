4c8d5c9fe9ec0dfe85b557d25509ec9175dfae4f
Luis Gonzalez <Luisargo2123@gmail.com>
2020-03-08 19:32:22 -0400

    Cleared some of the old XML code that was no longer being used

--- datr.py
+++ datr.py
@@ -1,14 +1,4 @@
-import requests
 import json
-from xml.etree import ElementTree
-print('hello world')
-r = requests.get('http://patcount.fiu.edu/garagecount.xml')
-tree = ElementTree.fromstring(r.content)
-print(tree[0])
-
-for elem in tree.iter():
-    print(elem.tag)
-    print(elem.text)
 
 
 exit = False
@@ -37,6 +27,10 @@ while(exit != True):
         print('Adding Food')
     elif(chooseActivity == '3'):
         print('Selecting Date')
+        priceFilter = input(
+            'What is your date budget (NOTE: this excludes the food budget)')
+        distanceFilter = input(
+            'How far are you willing to travel for this date? \n Close  (1) \n Medium (2)\n Far    (3) \n Epic   (4)')
         with open('dates.txt') as json_file:
             data = json.load(json_file)
             for p in data['dates']:
@@ -44,6 +38,15 @@ while(exit != True):
                 print('Cost: ' + p['cost'])
                 print('Distance: ' + p['distance'])
                 print('')
+
+        print('You are going on a date that costs no more than ' +
+              priceFilter + ' dollars ' 'and is a ' + distanceFilter + ' distance away')
+
+        # algorithm for randomization: first draf
+        # for loop that reads from json should use filters when selecting. Store filtered elements in array.
+        # Randomize from 0 - size of array pick date like that.
+        # Similar concept for food.
+
     else:
         print('Incorrect input: Valid input is: \n Add Date  (1) \n Add Food  (2) \n Pick Date (3)\n Exit      (4)')
 
