88ce4d4483403e8298ee3873dea746a319d2dd0d
Luis Gonzalez <Luisargo2123@gmail.com>
2020-04-19 21:27:44 -0400

    Continued playing around with a tkinter tutorial i found online

--- guisandbox.py
+++ guisandbox.py
@@ -8,8 +8,17 @@ lbl = Label(window, text="Datr", font=("Arial Bold", 20))  # Creates a label
 lbl.grid(column=0, row=0)  # Places the label
 
 lbl2 = Label(window, text="Datr", font=("Arial Bold", 20))  # Creates a label
-lbl2.grid(column=1, row=0)  # Places the label
+lbl2.grid(column=1, row=1)  # Places the label
 
-window.geometry('960x720')
+window.geometry('960x720')  # Setting window size
+
+
+def clicked():
+    lbl.configure(text="Butoon was clicked !!")
+
+
+btn = Button(window, text="Add Date", bg="orange", command=clicked)
+
+btn.grid(column=1, row=0)
 
 window.mainloop()
