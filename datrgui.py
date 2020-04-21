50d191abd11e347c12e4547446d93445bca0e1a0
Luis Gonzalez <Luisargo2123@gmail.com>
2020-04-21 14:35:53 -0400

    Added additional GUI functionality

--- datrgui.py
+++ datrgui.py
@@ -1,10 +1,32 @@
 from tkinter import *
+from tkinter import ttk
 
 window = Tk()
-window.config(height=500, width=500)
+# window.config(height=500, width=500)
 window.title("Datr")
 
-lbl = Label(window, text="Datr")
-lbl.place(relx=0.5, row=1, anchor=CENTER)
+tab_control = ttk.Notebook(window)
+window.geometry('960x720')
+
+tab1 = ttk.Frame(tab_control)
+tab2 = ttk.Frame(tab_control)
+tab3 = ttk.Frame(tab_control)
+
+tab_control.add(tab1, text='Home')
+tab_control.add(tab2, text='Manage Dates')
+tab_control.add(tab3, text='Manage Food')
+
+
+lbl1 = Label(tab1, text='Generate Date')
+lbl1.grid(column=0, row=0)
+
+lbl2 = Label(tab2, text='Manage Dates')
+lbl2.grid(column=0, row=0)
+
+lbl3 = Label(tab3, text='Manage Food')
+lbl3.grid(column=0, row=0)
+
+
+tab_control.pack(expand=1, fill='both')
 
 window.mainloop()
